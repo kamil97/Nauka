0bf9a4bbd66b7ace8cbf1d06ba7e8048ccf39f06
kamil <kamil@o2.pl>
2015-08-01 12:41:28 +0200

    rekurencja

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -2,8 +2,78 @@
 
 using namespace std;
 
+
+
+int SilniaIteracyjna(int a)
+{
+    int silnia=1;
+    for(int i=1; i<=a; i++)
+    {
+        silnia=silnia*i;
+
+    }return silnia;
+}
+
+
+
+
+int SilniaRekurencyjna(int a)
+{
+    if( a>1)
+    {
+        return a*SilniaRekurencyjna(a-1);
+    }else
+    {
+        return 1;
+    }
+}
+
+
+
+
+
+int Cloatz(int k)
+{
+    static int wywolania = 0;
+    cout<<"clo: "<< wywolania++<<" K: "<<k<<endl;
+    if(k==1)
+    {
+        return 1;
+    }
+
+    if(k%2==0)
+    {
+        return Cloatz(k/2);
+    }else if(k%2!=0)
+    {
+        return Cloatz(3*k+1);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a=5;
+    cout << SilniaIteracyjna(a)<<endl;
+
+    int b= 10;
+    cout << SilniaRekurencyjna(a)<<endl;
+    cout << SilniaRekurencyjna(10)<<endl;
+
+    int c = 6;
+    cout << Cloatz(c) << endl;
+
+
     return 0;
+
+
 }
