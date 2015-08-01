1375649ca6f34f56288e3a5eae0427c7f14ec0dc
kamil <kamil@o2.pl>
2015-08-01 12:42:46 +0200

    tablice framework

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void FillTable(int* table, int _size);
+void ShowTable(int* table, int _size);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int tab[5];
+
+    FillTable(tab,5);
+    ShowTable(tab,5);
+
     return 0;
+}
+
+
+void FillTable(int* table, int _size){
+    srand(time(NULL)); // podaje ziarno dla funkcji rand()
+    for(int i=0;i<_size;i++){
+        table[i] = rand()%100;
+    }
+}
+
+void ShowTable(int* table,int _size){
+    for(int i=0; i<_size;i++){
+        cout<<table[i]<<" ";
+    }
+    cout<<endl;
 }
