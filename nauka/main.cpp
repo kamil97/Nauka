dcca6fb79421324faf959ec103501c20b9f07992
kamil <kamil@o2.pl>
2015-08-04 21:00:56 +0200

    poczatki

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -2,8 +2,45 @@
 
 using namespace std;
 
+struct Wojownik
+{
+    //POLA
+    int hp;
+    int sila;
+    int zrecznosc;
+    string imie;
+
+    //Konstruktor
+    Wojownik()
+    {
+        hp =0;
+        sila=0;
+        zrecznosc=0;
+        imie = "";
+    }
+
+    Wojownik(string _imie)
+    {
+        imie = _imie;
+    }
+
+    //Metody
+    void Przedstaw_sie()
+    {
+        cout <<"nazywam sie "<<imie<<endl;
+    }
+
+};
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    Wojownik mirek("mirek"); //wywolanie konstruktora
+    Wojownik czesio("Czeslaw");
+
+    mirek.Przedstaw_sie();
+    czesio.Przedstaw_sie();
+
     return 0;
 }
