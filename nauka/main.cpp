979fc62a3ad9d24073302091a5d87441b1bfdb97
kamil <kamil@o2.pl>
2015-08-04 21:32:45 +0200

    nowe metody2

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -24,11 +24,51 @@ struct Wojownik
         imie = _imie;
     }
 
+    Wojownik(int _sila, int _zrecznosc, int _hp, string _imie)
+    {
+      sila = _sila;
+      zrecznosc = _zrecznosc;
+      hp = _hp;
+      imie = _imie;
+    }
+
     //Metody
     void Przedstaw_sie()
     {
         cout <<"nazywam sie "<<imie<<endl;
     }
+
+    bool Wejsc_na_konia(int wysokosc_konia)
+    {
+        if(wysokosc_konia>zrecznosc)
+        {
+            return false;
+        }else
+        {
+            return true;
+        }
+    }
+
+void Zycie()
+{
+    cout <<imie<<"zycie= "<<hp<<endl;
+}
+
+void Uderz(Wojownik przeciwnik)
+{
+    przeciwnik.Zycie();
+    if(przeciwnik.zrecznosc < this->zrecznosc)
+    {
+        przeciwnik.hp = przeciwnik.hp - this->sila;
+    }else
+    {
+        przeciwnik.hp = przeciwnik.hp- this-> sila/2;
+    }
+przeciwnik.Zycie();
+}
+
+
+
 
 };
 
@@ -42,5 +82,20 @@ int main()
     mirek.Przedstaw_sie();
     czesio.Przedstaw_sie();
 
+    //zagwostka
+    mirek = czesio;
+    mirek.Przedstaw_sie();
+
+
+    Wojownik kamil(10,15,20,"kamil");
+
+    cout << "wsiada na konia: "<<kamil.Wejsc_na_konia(5)<<endl;
+
+    cout << "wsiada na konia: "<<kamil.Wejsc_na_konia(30)<<endl;
+
+
+    Wojownik pawel(5,20,14,"lancelot pawel");
+    kamil.Uderz(pawel);
+    pawel.Uderz(kamil);
     return 0;
 }
