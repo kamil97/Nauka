8baa476443c2a0f43a5c2b340e127b385acbff35
kamil <kamil@o2.pl>
2015-08-11 18:33:59 +0200

    zliczanie n w tablicy

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -4,25 +4,72 @@
 
 using namespace std;
 
-void FillTable(int* table, int _size);
+void FillTableRandom(int* table, int _size);
+void FillTableWithIndexes(int* table, int _size, int startNumber = 0); // startNumber = 0 -> argument domyslny, mozemy wywolac ta funkcje jako func(tablica,20);
 void ShowTable(int* table, int _size);
+
+
+void Wypelnij_liczby_z_klawiatury(int* tablica, int rozmiar)
+{
+    for(int i=0; i<rozmiar; i++)
+    {
+        cout <<"podaj liczbe : "<<endl;
+        int liczby;
+        cin >>liczby;
+        tablica[i]=liczby;
+    }
+}
+
+
+
+
+int Sprawc_n(int n, int* tablica, int rozmiar)
+{
+    int ile_n = 0;
+    for(int i=0; i<rozmiar; i++)
+    {
+        if(tablica[i]==n)
+        {
+            ile_n=ile_n+1; // ile_n++;
+        }
+    } return ile_n;
+}
+
+
+
 
 int main()
 {
-    int tab[5];
 
-    FillTable(tab,5);
-    ShowTable(tab,5);
+
+
+    int rozmiar = 1000;
+    int tab[rozmiar];
+    FillTableRandom(tab,rozmiar);
+    int n=13;
+
+    cout << Sprawc_n(n, tab, rozmiar)<<endl;
+    ShowTable(tab, rozmiar);
+
+
 
     return 0;
 }
 
 
-void FillTable(int* table, int _size){
+void FillTableRandom(int* table, int _size){
     srand(time(NULL)); // podaje ziarno dla funkcji rand()
     for(int i=0;i<_size;i++){
         table[i] = rand()%100;
     }
+}
+
+void FillTableWithIndexes(int* table, int _size, int startNumber){
+
+for(int i=0; i<_size; i++)
+{
+    table[i] = startNumber++;
+}
 }
 
 void ShowTable(int* table,int _size){
@@ -30,4 +77,5 @@ void ShowTable(int* table,int _size){
         cout<<table[i]<<" ";
     }
     cout<<endl;
+
 }
