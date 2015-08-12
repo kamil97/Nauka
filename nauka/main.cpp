850fc70596554afedf23eed090df36d72a2d32a0
kamil <kamil@o2.pl>
2015-08-12 16:06:07 +0200

    stringi

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string str = "abcdef";
+
+    cout << "to bedzie enter \nnowa linia ";
+
+    for(int i=0; i<str.size(); i++)
+    {
+        if(str[i]== 'b'){
+            cout << "MAMY B ";
+        }
+        if(str[i]>= 'e'){
+            cout << "WIEKSZE E ";
+        }
+    if(str[i]==0x61){
+            cout << " ";
+        }
+        cout << str[i]<<endl;
+    }
     return 0;
 }
