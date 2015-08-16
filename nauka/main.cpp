fd18663d96ed620fa0bcd72c8ec1794ea31b4d91
kamil <kamil@o2.pl>
2015-08-16 12:28:15 +0200

    zamiana cyfr na kropki

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+
+bool zamien_cyfre_na_kropke(string &zamiana)
+{
+    bool zamiana_nastapila=false;
+    for(int i=0; i<zamiana.size(); i++)
+    {
+        if((zamiana[i]>='0')&&(zamiana[i]<='9'))// dwa nawiasy
+        {
+            zamiana[i]='.';
+            zamiana_nastapila=true;
+        }
+
+    }
+    return zamiana_nastapila;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
     string str = "abcdef";
@@ -21,5 +48,28 @@ int main()
         }
         cout << str[i]<<endl;
     }
+
+    cout <<endl;
+    string test1="asdf1234mmnbv6jk";
+    string test2 = "1234567";
+    string test3 = "blhoglhhj";
+    cout<<test1<<endl;
+    cout <<test2<<endl;
+    cout<<test3<<endl;
+    cout<<zamien_cyfre_na_kropke(test1)<<endl;
+    cout<<zamien_cyfre_na_kropke(test2)<<endl;
+    cout<<zamien_cyfre_na_kropke(test3)<<endl;
+    cout <<test1<<endl;
+    cout <<test2<<endl;
+    cout<<test3<<endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
