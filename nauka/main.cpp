53ac9c0682961d065d87fcb32074ab7f13538a75
kamil <kamil@o2.pl>
2015-08-19 21:01:33 +0200

    denamiczne tablice

--- nauka/main.cpp
+++ nauka/main.cpp
@@ -2,8 +2,36 @@
 
 using namespace std;
 
+
+void Dynamiczna_tablica(int n)
+{
+    int tablica[n];
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    int a = 5;
+    int tab[a];
+
+    int *pInt;
+    pInt = new int[a];
+
+    for(int i= 0; i<a; i++){
+
+            tab[i]=i+5;
+            pInt[i]=i+20;
+    }
+
+    cout<<*tab<<endl;
+    cout<<tab[0]<<endl;
+    cout<<*(tab+2)<<endl;
+    cout<<tab[2]<<endl;
+
+    cout<<*pInt<<endl;
+    cout<<pInt[0]<<endl;
+    cout<<*(pInt+2)<<endl;
+    cout<<pInt[2]<<endl;
+
     return 0;
 }
